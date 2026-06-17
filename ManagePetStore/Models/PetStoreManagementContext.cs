609ee2476ec3e81285e3beb8daefb7136f2e4bd7
Tran Duong <144971476+duontq@users.noreply.github.com>
2026-06-17 19:43:23 +0700

    adding dbcontext (2 table supplier and supplier categories)

--- ManagePetStore/Models/PetStoreManagementContext.cs
+++ ManagePetStore/Models/PetStoreManagementContext.cs
@@ -67,6 +67,10 @@ public partial class PetStoreManagementContext : DbContext
 
     public virtual DbSet<StockMovementDetail> StockMovementDetails { get; set; }
 
+    public virtual DbSet<Supplier> Suppliers { get; set; }
+
+    public virtual DbSet<SupplierCategory> SupplierCategories { get; set; }
+
     public virtual DbSet<User> Users { get; set; }
 
     public virtual DbSet<Voucher> Vouchers { get; set; }
